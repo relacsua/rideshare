ba603faedac317de164b548a70876947bb22c03c
Muhammad Muneer <muhammad.muneer91@gmail.com>
2015-09-17 08:22:27 +0800

    add statement to show how debugbar can be used

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use DB;
+use Debugbar;
 
 class UserController extends Controller
 {
@@ -18,8 +19,15 @@ class UserController extends Controller
     public function index($name)
     {
         // dd (DB::connection()->getPdo());
-        $faculty = DB::select("SELECT s.faculty FROM student s WHERE s.name=? AND ROWNUM=1", [$name]);
-        return view('welcome', ['faculty' => $faculty[0]->faculty]);
+        $faculty = DB::select("SELECT s.faculty FROM student s WHERE s.name=upper(?) AND ROWNUM=1", [$name]);
+        Debugbar::info($faculty);
+        // Debugbar::error('Error!');
+        // Debugbar::warning('Watch out…');
+        // Debugbar::addMessage('Another message', 'mylabel');
+        if(empty($faculty))
+            return view('welcome', ['faculty' => 'Not Found']);
+        else
+            return view('welcome', ['faculty' => $faculty[0]->faculty]);
     }
 
 }
--- config/app.php
+++ config/app.php
@@ -196,7 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        
     ],
 
 ];
