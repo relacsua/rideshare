bbb20df6ab6cb72466b5bfd9878f06bee4113a0c
Muhammad Muneer <muhammad.muneer91@gmail.com>
2015-10-18 14:01:51 +0800

    usual login functionality added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,56 +63,25 @@ class UserController extends Controller
         }
     }
 
-    // public function newProfile(Request $request) {
-    // 	$user = Session::get('user');
-    // 	Session::forget('user');
-
-    // 	$user_details = array(
-    // 		'email' => $user->getEmail(),
-    // 		'name' => $user->getName(),
-    // 		'gender' => strtoupper($user->user['gender']),
-    // 		'avatar' => $user->avatar_original,
-    // 		'token' => $user->token,
-    // 		'id' => $user->id
-    // 	);
-    	
-    // 	return view('profile.create', ['user' => $user_details]);
-    // }
-
-   //  public function createProfile(Request $request) {
-			
-			// $inputs = Request::all();
-		 //  $email = $inputs['email'];
-		 //  $name = $inputs['name'];
-		 //  $age = $inputs['age'];
-		 //  $gender = $inputs['gender'];
-		 //  $avatar = $inputs['avatar'];
-		 //  $password = Hash::make($inputs['password']);
-		 //  $token = $inputs['token'];
-		 //  $id = $inputs['id'];
-		 //  $isDriver = array_key_exists('isDriver', $inputs);
-		  
-			// DB::insert("INSERT INTO Person (email, name, gender, avatar, age, password) VALUES (?,?,?,,?,?)", [$email, $name, $gender, $avatar, $age, $password]);
-			// DB::insert("INSERT INTO Has_Profile (token, userID, email) VALUES (?,?,?)", [$token, $id, $email]);
-			
-			// if($isDriver) {
-			// 	$carPlateNo = $inputs['carPlateNo'];
-			// 	$carModel = $inputs['carModel'];
-			// 	$licenceNo = $inputs['licenceNo'];
-			// 	$numSeats = $inputs['numSeats'];
-				
-			// 	DB::insert("INSERT INTO Owns_Car (carPlateNo, carModel, ownerLicenseNo, ownerEmail, numSeats) VALUES (?,?,?,?,?)", [$carPlateNo, $carModel, $licenceNo, $email, $numSeats]);
-			// }
-
-   //  }
-
     public function login(Requests\LoginRequest $request) {
     	$email = $request->email;
       $password = $request->password;
       
       try {
-          // $user = DB::select('SELECT * FROM PERSON p WHERE p.email=? AND p.password=?', [$email, $password]);
-          dd('hello worked');
+          $user = DB::select('SELECT p.password FROM PERSON p WHERE p.email=?', [$email]);
+          
+          if(empty($user)) {
+          	return Redirect::to('/welcome')->with('error', 'Invalid email address/password');
+          } else {
+						$hashed_password = $user[0]->password;
+						if(Hash::check($password, $hashed_password)) {
+							Session::put('email', $email);
+							return Redirect::to('/');
+							} else {
+							return Redirect::to('/welcome')->with('error', 'Invalid email address/password');
+							}
+          }
+
       } catch (\Illuminate\Database\QueryException $e) {
           return var_dump($e);
       }
