4ac221826f4cea72fc54bcdf247cdb843c0e4f20
Lee Chi Cheng Daniel <daniel.lcc.2741991@gmail.com>
2015-10-22 22:50:08 +0800

    Trivia

--- database/schema/Carpooling_DropTables.sql
+++ database/schema/Carpooling_DropTables.sql
@@ -1,10 +1,10 @@
-DROP TABLE Driver_Ride;
-DROP TABLE Passenger;
-DROP TABLE Owns_Car;
-DROP TABLE Has_Profile;
-DROP TABLE Person;
-
 DROP TRIGGER isValidDriverAndRideCapacity;
 DROP TRIGGER isValidRideTiming;
 DROP TRIGGER hasEnoughCredit;
 DROP TRIGGER isRideTooCloseToOthers;
+
+DROP TABLE Passenger;
+DROP TABLE Owns_Car;
+DROP TABLE Has_Profile;
+DROP TABLE Driver_Ride;
+DROP TABLE Person;
