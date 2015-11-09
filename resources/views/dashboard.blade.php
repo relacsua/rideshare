cbb69817337f9b6f202654255df47e6b8d387f49
Muhammad Muneer <muhammad.muneer91@gmail.com>
2015-11-09 22:59:56 +0800

    style the buttons in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,21 @@
 @extends('home')
 
+@section('style')
+	<style type="text/css">
+		.action-btn, .action-btn:hover {
+			background-color: #105b63;
+		    border: 0;
+		    border-radius: 0;
+		    color: #ffffff;
+		    width: 100%;
+		    padding: 20px;
+		}
+		.action-btn .glyphicon {
+			font-size: 40px;
+		}
+	</style>
+@stop
+
 @section('content')
 	<div class="row">
 		<div class="col-lg-12">
@@ -8,8 +24,19 @@
 				<span>Dashboard</span>
 			</h1>
 		</div>
-		<a class="btn rideshare-btn" href="/rides/new">Post a ride</a>
-		<a class="btn rideshare-btn" href="/rides/search">Join a ride</a>
-		<p>We can place charts and graphs to show some <b>DATA ANALYSIS</b>. How awesome is that ??</p>
+		<div class="row">
+			<div class="col-md-4 col-md-offset-2">
+				<a class="btn action-btn" href="/rides/new">
+					<span class="glyphicon glyphicon-road" aria-hidden="true"></span>
+					<h3>Post a ride</h3>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a class="btn action-btn" href="/rides/search">
+					<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+					<h3>Join a ride</h3>
+				</a>
+			</div>
+		</div>
 	</div>
 @stop
